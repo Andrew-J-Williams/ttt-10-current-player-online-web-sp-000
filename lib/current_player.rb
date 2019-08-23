6698cc466a8d4b6f6ac15f52be664ab20e7d5bfc
Andrew Williams <1williamsaj@gmail.com>
2019-08-23 01:23:36 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
 counter = 0
 
 board.each do |turn|
-  if board[turn.to_i] == "X" || board[turn] == "O"
+  if board[turn.to_i] == "X" || board[turn.to_i] == "O"
     counter +=1
   end
 end
