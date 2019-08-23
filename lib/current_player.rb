f6734267060f4138321292f3b6f5c39bbf3c5d57
Andrew Williams <1williamsaj@gmail.com>
2019-08-23 01:42:08 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,10 @@
 def turn_count(board)
   counter = 0
 
-  board.each do |turn|
-    if turn == "X" || turn == "O"
+  board.each do |turn| #Takes the values from the board array, plugs them into the local variable 'turn'
+    if turn == "X" || turn == "O" #Checks to see if the space in the array is occupied by either 'X' or 'O'.
       counter += 1
+    end
   end
 
 end
